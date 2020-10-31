6c8c5846873d9fe86ad9bd0ffe74999070c1006f
faberbg <faber@Davids-MacBook-Pro.local>
2020-10-31 20:24:00 +0100

    database

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database
+{}
+    private $hostname = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $dbname;
+    private $dblink;
+    private $result;
+    private $records;
+    private $affected;
+}
+
+function __construct($par_dbname)
+{
+
+}
+
+
+?>
